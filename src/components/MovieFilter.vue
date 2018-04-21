31d2e1d700f28960bc11b5aa6d1a98d4c044d68d
Danny Avery <danny.w.avery@gmail.com>
2018-04-21 16:41:16 -0600

    Create single file components

---
+++ src/components/MovieFilter.vue
@@ -0,0 +1,34 @@
+<template>
+	<div id="movie-filter">
+		<h2>Filter Results</h2>
+		<div class="filter-group">
+			<check-filter v-for="genre in genres" 
+				:title="genre"
+				@check-filter="checkFilter">
+			</check-filter>
+		</div>
+	</div>	
+</template>
+
+<script>
+	import genres from '../util/genres';
+	import CheckFilter from './CheckFilter.vue';
+
+	export default {
+		data() {
+			return {
+				genres
+			};
+		},
+
+		methods: {
+			checkFilter(category, title, checked) {
+				this.$emit('check-filter', category, title, checked);
+			}
+		},
+
+		components: {
+			CheckFilter
+		}
+	}
+</script>

b4394ad8cb3cd5c5c3b9c657fccb8b8f8e6226b9
Danny Avery <danny.w.avery@gmail.com>
2018-04-23 19:26:04 -0600

    Display movie item in Detail.vue

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,11 +1,29 @@
 <template>
-	<div>Hello World</div>	
+	<div v-if="movie" id="detail">
+		<movie-item :movie="movie.movie" :sessions="[]"></movie-item>
+	</div>
 </template>
 
 <script>
+	import MovieItem from './MovieItem.vue';
+
 	export default {
+		props: [ 'movies' ],
+
+		components: {
+			MovieItem
+		},
+
 		created() {
+			// used for testing id being passed
 			console.log(this.$route.params.id);
+		},
+
+		computed: {
+			movie() {
+				let movie = this.movies.find(movie => movie.id === this.$route.params.id);
+				return movie ? movie : null;
+			}
 		}
 	}
 </script>
